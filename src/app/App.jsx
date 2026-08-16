a36f41d8a1f6b26e6e2c666fbe05932d94a523ba
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-16 13:11:46 -0400

    Enable global game audio bridge

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { FullscreenShell } from '../components/FullscreenShell.jsx';
 import { ChronoRouter } from '../components/ChronoRouter.jsx';
 import { AppStatus } from '../components/AppStatus.jsx';
+import { GameAudioBridge } from '../components/GameAudioBridge.jsx';
 
 export function App() {
   return (
     <FullscreenShell>
       <ChronoRouter />
       <AppStatus />
+      <GameAudioBridge />
     </FullscreenShell>
   );
 }
