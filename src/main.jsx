5e8ff20310d69c2281565b6249d17684934e74ae
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-16 15:17:44 -0400

    Load Chronicle combat stat styles

--- src/main.jsx
+++ src/main.jsx
@@ -30,6 +30,7 @@ import './styles/era-challenges.css';
 import './styles/era-daily.css';
 import './styles/evolutions.css';
 import './styles/rift-boss.css';
+import './styles/chronicle-stats.css';
 import { registerServiceWorker } from './core/registerServiceWorker.js';
 import { installGameTimerGuard } from './core/gameTimers.js';
 
