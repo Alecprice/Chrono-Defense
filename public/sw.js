5a525111f2be00a16ffb3e9680af0edcd73bb1bd
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-17 02:05:57 -0400

    Bump offline cache for all-era Junior progression

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='chrono-defense-shell-v24';
+const CACHE='chrono-defense-shell-v25';
 const CORE=['/','/index.html','/manifest.webmanifest','/precache-manifest.json'];
 async function precache(){
   const cache=await caches.open(CACHE);
