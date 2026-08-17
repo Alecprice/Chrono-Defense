ab457230bd377981b1733f49a1089e483e3abd07
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-17 01:37:57 -0400

    Load kid-safe action styles

--- src/main.jsx
+++ src/main.jsx
@@ -46,6 +46,7 @@ import './styles/stone-route.css';
 import './styles/junior-mode.css';
 import './styles/junior-rewards.css';
 import './styles/resume-battle.css';
+import './styles/kid-safe-actions.css';
 import { registerServiceWorker } from './core/registerServiceWorker.js';
 import { installGameTimerGuard } from './core/gameTimers.js';
 
