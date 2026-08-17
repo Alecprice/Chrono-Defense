63ba9d89644a6e9832d99075ad1b98598fc4a65c
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-17 01:42:31 -0400

    Bump offline cache for training campaign

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='chrono-defense-shell-v20';
+const CACHE='chrono-defense-shell-v21';
 const CORE=['/','/index.html','/manifest.webmanifest','/precache-manifest.json'];
 async function precache(){
   const cache=await caches.open(CACHE);
