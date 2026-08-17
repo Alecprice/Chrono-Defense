50063cc0476e143151eea1689fd246e9b9b586d3
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-16 23:51:13 -0400

    Load Junior Mode reward styles

--- src/main.jsx
+++ src/main.jsx
@@ -44,6 +44,7 @@ import './styles/map-objectives.css';
 import './styles/objective-checklist.css';
 import './styles/stone-route.css';
 import './styles/junior-mode.css';
+import './styles/junior-rewards.css';
 import { registerServiceWorker } from './core/registerServiceWorker.js';
 import { installGameTimerGuard } from './core/gameTimers.js';
 
