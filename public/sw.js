4d2bcbc5c621577abea52054b64f45abcbba29fa
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-17 01:17:39 -0400

    Bump offline cache for all-era battle resume

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='chrono-defense-shell-v16';
+const CACHE='chrono-defense-shell-v17';
 const CORE=['/','/index.html','/manifest.webmanifest','/precache-manifest.json'];
 async function precache(){
   const cache=await caches.open(CACHE);
