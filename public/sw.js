de07c8eb88ec3692d8addba58acb4086614ada25
Alec Price <52548136+Alecprice@users.noreply.github.com>
2026-08-17 01:38:57 -0400

    Bump offline cache for kid-safe actions

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='chrono-defense-shell-v19';
+const CACHE='chrono-defense-shell-v20';
 const CORE=['/','/index.html','/manifest.webmanifest','/precache-manifest.json'];
 async function precache(){
   const cache=await caches.open(CACHE);
